0d72ef3e3bc225286fb0caf18b43e294a72b26af
Fábio Ceolin <fabioceolin98@gmail.com>
2020-09-07 14:05:32 -0300

    Routes added

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,7 @@
-import styled from "styled-components";
-import { shade } from "polished";
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
 
-import signInBackgroundImg from "../../assets/sign-in-background.png";
+import signInBackgroundImg from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -14,8 +14,28 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   place-content: center;
+
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
@@ -34,7 +54,7 @@ export const Content = styled.div`
       transition: color 0.2s;
 
       &:hover {
-        color: ${shade(0.2, "#f4ede8")};
+        color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
@@ -54,7 +74,7 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, "#ff9000")};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
 `;
